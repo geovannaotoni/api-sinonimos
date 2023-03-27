216e1e2c2f29fcb3e177465c5dd98a54f326a291
Geovanna Otoni <geovannaotoni@gmail.com>
2023-03-27 16:54:57 -0300

    finalizacao

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Sinônimos
-Projeto Pessoal de um aplicação que exibe alguns sinônimos da língua portuguesa da palavra pesquisada pela pessoa usuária, a partir da API DicioAPI.<br><br>
+Projeto Pessoal de um aplicação que exibe alguns sinônimos da língua portuguesa da palavra pesquisada pela pessoa usuária, a partir da API DicioAPI. Também foram utilizadas as bibliotecas NPM Clipboard Copy para a pessoa usuária poder copiar o sinônimo para a área de transferência e SweetAlert para alertar que a palavra foi copiada com sucesso.<br><br>
 <img src="">
 
 ## :computer: Visualize este projeto:
--- src/script.js
+++ src/script.js
@@ -1,9 +1,27 @@
 const apiURL = 'https://dicio-api-ten.vercel.app/v2/sinonimos/';
 const inputWord = document.getElementById('input-word');
 const searchBtn = document.getElementById('search-btn');
-const resultList = document.getElementById('result')
+const resultList = document.getElementById('result');
+const main = document.querySelector('main');
+
+import clipboardCopy from 'clipboard-copy';
+import Swal from 'sweetalert2';
+
+const loadingText = () => {
+  const p = document.createElement('p');
+  p.innerText = 'carregando...';
+  p.classList.add('loading');
+  main.appendChild(p);
+}
+
+const removeLoadingText = () => {
+  const p = document.querySelector('.loading');
+  p.remove();
+}
 
 const seachSinonimo = async () => {
+  loadingText();
+  resultList.innerHTML = '';
   const inputValue = inputWord.value;
   const response = await fetch(`${apiURL}${inputValue}`);
   const data = await response.json();
@@ -12,5 +30,17 @@ const seachSinonimo = async () => {
     li.innerHTML = data[index];
     resultList.appendChild(li);
   }
+  removeLoadingText();
 }
 searchBtn.addEventListener('click', seachSinonimo);
+
+inputWord.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    seachSinonimo();
+  }
+});
+
+resultList.addEventListener('click', (event) => {
+  clipboardCopy(event.target.innerText);
+  Swal.fire('Sinônimo copiado!')
+})
