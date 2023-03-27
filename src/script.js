85c6eeab8a8f454c71780301bb832a11a88b5ee6
Geovanna Otoni <geovannaotoni@gmail.com>
2023-03-27 13:52:12 -0300

    estrutura

--- src/script.js
+++ src/script.js
@@ -1,11 +1,16 @@
 const apiURL = 'https://dicio-api-ten.vercel.app/v2/sinonimos/';
-const inputValue = 'adquirir'
+const inputWord = document.getElementById('input-word');
+const searchBtn = document.getElementById('search-btn');
+const resultList = document.getElementById('result')
 
 const seachSinonimo = async () => {
+  const inputValue = inputWord.value;
   const response = await fetch(`${apiURL}${inputValue}`);
   const data = await response.json();
   for (let index = 0; index < data.length; index += 1) {
-    console.log(data[index]);
+    const li = document.createElement('li');
+    li.innerHTML = data[index];
+    resultList.appendChild(li);
   }
 }
-seachSinonimo();
+searchBtn.addEventListener('click', seachSinonimo);
